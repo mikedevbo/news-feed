2de9c6bc62cc5f59c919a734b8e927a69470f8df
mikedevbo <mike@devbo.pl>
2022-06-18 20:07:54 +0200

    get fake tweets from api

--- src/Server/Controllers/NewsFeedController.cs
+++ src/Server/Controllers/NewsFeedController.cs
@@ -21,4 +21,45 @@ public class NewsFeedController : ControllerBase
             new Account { Id = "User_9", GroupName = "Sport" }
         };
     }
+
+    [HttpGet("GetTweets")]
+    public IEnumerable<Tweet> GetAccounts(string accountId)
+    {
+        var result = new List<Tweet>();
+
+        switch (accountId)
+        {
+            case "User_1":
+                result.AddRange(new List<Tweet>
+                {
+                    new Tweet { AccountId = accountId, Text = "u1_t1" },
+                    new Tweet { AccountId = accountId, Text = "u1_t2" },
+                    new Tweet { AccountId = accountId, Text = "u1_t3" }
+                });
+                break;
+            case "User_2":
+                result.AddRange(new List<Tweet>
+                {
+                    new Tweet { AccountId = accountId, Text = "u2_t1" },
+                    new Tweet { AccountId = accountId, Text = "u2_t2" },
+                    new Tweet { AccountId = accountId, Text = "u2_t3" },
+                    new Tweet { AccountId = accountId, Text = "u2_t4" }
+                });
+                break;
+            case "User_3":
+                result.AddRange(new List<Tweet>
+                {
+                    new Tweet { AccountId = accountId, Text = "u3_t1" },
+                    new Tweet { AccountId = accountId, Text = "u3_t2" },
+                    new Tweet { AccountId = accountId, Text = "u3_t3" },
+                    new Tweet { AccountId = accountId, Text = "u3_t4" },
+                    new Tweet { AccountId = accountId, Text = "u3_t5" }
+                });
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
 }
---
+++ src/Shared/Tweet.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsFeed.Shared
+{
+    public class Tweet
+    {
+        public string AccountId { get; set; } = "";
+
+        public string Text { get; set; } = "";
+    }
+}
