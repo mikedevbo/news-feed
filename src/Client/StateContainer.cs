54b1702c9b5b5f96faaac05d9d03fe3dde6261cb
mikedevbo <mike@devbo.pl>
2022-06-18 14:33:14 +0200

    show tweets with fake data

---
+++ src/Client/StateContainer.cs
@@ -0,0 +1,16 @@
+﻿public class StateContainer
+{
+    private string selectedAccountId = "";
+
+    public string SelectedAccountId
+    {
+        get => selectedAccountId;
+        set
+        {
+            selectedAccountId = value;
+            this.OnSelectedAccountIdChange?.Invoke(this.selectedAccountId);
+        }
+    }
+
+    public event Action<string>? OnSelectedAccountIdChange;
+}
