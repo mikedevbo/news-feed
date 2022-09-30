22d8b5db99c6c4be7fc5cfc86712507e1943f7eb
mikedevbo <mike@devbo.pl>
2022-09-30 15:55:38 +0200

    add method signature

--- src/Server/Models/ITwitterRepository.cs
+++ src/Server/Models/ITwitterRepository.cs
@@ -1,9 +1,12 @@
 ﻿using NewsFeed.Shared;
+using TwitterSharp.Response.RTweet;
 
 namespace NewsFeed.Server.Models
 {
     public interface ITwitterRepository
     {
+        Task SaveTweets(int UserId, List<Tweet> Tweets);
+
         IList<TweetDto> GetDownloadedTweets(string accountId);
     }
 }
--- src/Server/Models/TwitterRepository.cs
+++ src/Server/Models/TwitterRepository.cs
@@ -1,11 +1,15 @@
 ﻿using NewsFeed.Shared;
-using TwitterSharp.Request.AdvancedSearch;
-using TwitterSharp.Request.Option;
+using TwitterSharp.Response.RTweet;
 
 namespace NewsFeed.Server.Models
 {
     public class TwitterRepository : ITwitterRepository
     {
+        public Task SaveTweets(int UserId, List<Tweet> Tweets)
+        {
+            throw new NotImplementedException();
+        }
+
         public IList<TweetDto> GetDownloadedTweets(string accountId)
         {
             return new List<TweetDto>();
