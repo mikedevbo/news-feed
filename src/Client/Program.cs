f4776078675a1a99ecbafbe2d4f5a97b279adb56
mikedevbo <mike@devbo.pl>
2022-10-08 10:42:06 +0200

    set up MudBlazor component library

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using NewsFeed.Client;
 using NewsFeed.Shared;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<StateContainer>();
 builder.Services.AddScoped(sp => new NewsFeedTwitterApiClient(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }));
 
