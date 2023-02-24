9219c587bcdc353c4363ee7c6a7ff368f09ee7ec
mikedevbo <mike@devbo.pl>
2023-02-24 16:38:01 +0100

    remove unused code

--- src/Server/Twitter/Database/Entity/TwitterGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewsFeed.Server.Models.Twitter.Entity
-{
-    public record TwitterGroup(int Id, string Name, int AccountId)
-    {
-        public TwitterGroup() : this(default, string.Empty, default) { }
-
-        public TwitterGroup(string Name, int AccountId) : this(default, Name, AccountId) { }
-    }
-}
