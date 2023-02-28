d6f0a212a3da078301ba509f25e1d050dc43a2ff
mikedevbo <mike@devbo.pl>
2023-02-28 16:13:24 +0100

    get tweets downloading state

--- src/Server/Twitter/Mediating/GetDownloadingTweetsState.cs
+++ src/Server/Twitter/Mediating/GetDownloadingTweetsState.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Dapper;
+using MediatR;
+using Microsoft.Data.SqlClient;
 using NewsFeed.Shared.Twitter;
 
 namespace NewsFeed.Server.Twitter.Mediating
@@ -19,12 +21,22 @@ namespace NewsFeed.Server.Twitter.Mediating
 
         public async Task<string> Query(GetDownloadingTweetsState request)
         {
-            return @"<Root>
-	<UserDownladTweetsState>
-		<UserId>1</UserId>
-		<IsDownloadingTweets>true</IsDownloadingTweets>
-	</UserDownladTweetsState>
-</Root>";
+            if (request.UsersIds.Count <= 0)
+            {
+                return "<Root />";
+            }
+
+            var sql = @"select u.Id as UserId, u.IsTweetsDownloading as IsDownloadingTweets
+from dbo.TwitterUsers u
+where u.Id in @UsersIds
+for xml path ('UserDownladTweetsState'), root('Root')";
+
+            using var connection = new SqlConnection(configuration.GetValue<string>(Constants.ConnectionStringPersistenceKey));
+            await connection.OpenAsync();
+            var data = await connection.QueryAsync<string>(sql, new { request.UsersIds });
+            return data.Any()
+                ? string.Join("", data)
+                : "<Root />";
         }
     }
 }
